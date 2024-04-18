5cd8db7d883a7166760141699305e470e4820f4e
Maggie Eminizer <margaret.eminizer@gmail.com>
2024-04-18 17:40:25 -0400

    i/o from and to Girder is working locally

--- imqcam/ttt_ti64/ttt_ti64.py
+++ imqcam/ttt_ti64/ttt_ti64.py
@@ -8,15 +8,20 @@ from dagster import op, graph, In, Out, MetadataValue
 from dagster_celery_k8s import celery_k8s_job_executor
 import pandas as pd
 import matplotlib.pyplot as plt
+from ..girder.girder_handling import (
+    get_dataframe_from_girder_csv_file,
+    write_dataframe_to_girder_file,
+)
 from . import SinglePointSTK as SP
 
 
 @op(ins={"input_path": In(str)}, out={"output_path": Out(str)})
 def run_ttt_ti64(context, input_path: str) -> str:
+    # Read the input file from Girder into a dataframe
+    df = get_dataframe_from_girder_csv_file(input_path)
+    # Rename the columns in the dataframe as read from the file
+    df.columns = ["z position", "Time (s)", "Temperature"]
     # Specify the position time/temperature history
-    df = pd.read_csv(
-        input_path, header=0, names=["z position", "Time (s)", "Temperature"]
-    )
     df = df.pivot(index="Time (s)", columns="z position", values="Temperature")
     # Read the data into lists
     Times = list(df.index.values)
@@ -59,21 +64,22 @@ def run_ttt_ti64(context, input_path: str) -> str:
     tz_NY = pytz.timezone("America/New_York")
     # Get the current time in New York
     now = datetime.now(tz_NY)
-    # Set the outputfilenama
+    # Set the outputfilename
     out_file = (
         input_path.removesuffix(".csv")
         + "_out_"
         + now.strftime("%d%m%y_%H%M%S")
         + ".csv"
     )
-    dfout.to_csv(out_file)
+    # Write the dataframe back out as a new file in Girder
+    write_dataframe_to_girder_file(out_file, dfout)
     return out_file
 
 
 @op(ins={"input_path": In(str)})
 def plot_ttt_ti64(context, input_path: str) -> None:
     # read the input result file
-    df = pd.read_csv(input_path).drop(columns=["Unnamed: 0"])
+    df = get_dataframe_from_girder_csv_file(input_path)
     # Make the plot
     f, ax = plt.subplots(2, 1, figsize=(2 * 4, 6), sharex=True)
     for colname in df.columns:
@@ -110,7 +116,9 @@ ttt_ti64_job = ttt_ti64_graph.to_job(
     config={
         "ops": {
             "run_ttt_ti64": {
-                "inputs": {"input_path": "/imqcam_local_data/sample_z_time_temp.csv"},
+                "inputs": {
+                    "input_path": "Eminizer/webler_ttt_ti64_example/sample_z_time_temp.csv"
+                },
             },
         },
         "execution": {
