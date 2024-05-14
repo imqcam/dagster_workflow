64e42352dafc641d2c0760b5b01eb9cb4eb72712
Maggie Eminizer <margaret.eminizer@gmail.com>
2024-05-14 10:44:50 -0400

    some style edits to the ttt_ti64 code

--- imqcam/ttt_ti64/ttt_ti64.py
+++ imqcam/ttt_ti64/ttt_ti64.py
@@ -1,9 +1,10 @@
 # imports
-import pytz
+import os
 import pathlib
 import base64
 from io import BytesIO
 from datetime import datetime
+import pytz
 from dagster import op, graph, In, Out, MetadataValue
 from dagster_celery_k8s import celery_k8s_job_executor
 import pandas as pd
@@ -16,7 +17,10 @@ from . import SinglePointSTK as SP
 
 
 @op(ins={"input_path": In(str)}, out={"output_path": Out(str)})
-def run_ttt_ti64(context, input_path: str) -> str:
+def run_ttt_ti64(context, input_path: str) -> str: # pylint: disable=unused-argument
+    """ Reads the input file from Girder, runs the analysis, writes the output csv file
+    back to Girder, and returns the path to the file that was outputted
+    """
     # Read the input file from Girder into a dataframe
     df = get_dataframe_from_girder_csv_file(input_path)
     # Rename the columns in the dataframe as read from the file
@@ -24,46 +28,47 @@ def run_ttt_ti64(context, input_path: str) -> str:
     # Specify the position time/temperature history
     df = df.pivot(index="Time (s)", columns="z position", values="Temperature")
     # Read the data into lists
-    Times = list(df.index.values)
+    times = list(df.index.values)
     zpos = list(df.columns.values)
     # Convert z from meters to millimeters
     zpos = [10**3 * x for x in zpos]
     # compute the fractions of microstructure constituents for each position
-    Fraca = []
-    FracGB = []
-    FracBW = []
-    FracCOL = []
-    FracMASS = []
-    FracMART = []
-    Tlath = []
+    frac_alpha = []
+    frac_gb = []
+    frac_bw = []
+    frac_col = []
+    frac_mass = []
+    frac_mart = []
+    tlath = []
     for c in df.columns:
-        TempC = df[c]
+        temp_c = df[c]
         # Run the single point function
-        Thermal = SP.ThermalStep(Times, TempC)
-        Fraca.append(Thermal[0])
-        FracGB.append(Thermal[1])
-        FracBW.append(Thermal[2])
-        FracCOL.append(Thermal[3])
-        FracMASS.append(Thermal[4])
-        FracMART.append(Thermal[5])
-        Tlath.append(Thermal[6])
+        thermal = SP.ThermalStep(times, temp_c)
+        frac_alpha.append(thermal[0])
+        frac_gb.append(thermal[1])
+        frac_bw.append(thermal[2])
+        frac_col.append(thermal[3])
+        frac_mass.append(thermal[4])
+        frac_mart.append(thermal[5])
+        tlath.append(thermal[6])
     # This output is at the end of the time-temperature history
     output = {
         "zpos[mm]": zpos,
-        "fracalpha": Fraca,
-        "fracGB": FracGB,
-        "fracBW": FracBW,
-        "fracCOL": FracCOL,
-        "fracMASS": FracMASS,
-        "fracMart": FracMART,
-        "tlath": Tlath,
+        "fracalpha": frac_alpha,
+        "fracGB": frac_gb,
+        "fracBW": frac_bw,
+        "fracCOL": frac_col,
+        "fracMASS": frac_mass,
+        "fracMart": frac_mart,
+        "tlath": tlath,
     }
     dfout = pd.DataFrame(output)
-    # Output to a csv file using the time/temp history file and current date/time as a unique identifier
+    # Output to a csv file using the time/temp history file and current date/time 
+    # as a unique identifier
     # Get the timezone object for New York (Eastern time zone)
-    tz_NY = pytz.timezone("America/New_York")
+    tz_ny = pytz.timezone(os.getenv("TZ","America/New_York"))
     # Get the current time in New York
-    now = datetime.now(tz_NY)
+    now = datetime.now(tz_ny)
     # Set the outputfilename
     out_file = (
         input_path.removesuffix(".csv")
@@ -78,6 +83,7 @@ def run_ttt_ti64(context, input_path: str) -> str:
 
 @op(ins={"input_path": In(str)})
 def plot_ttt_ti64(context, input_path: str) -> None:
+    """ Make a plot of the result and output it to the op's metadata as markdown """
     # read the input result file
     df = get_dataframe_from_girder_csv_file(input_path)
     # Make the plot
@@ -103,6 +109,8 @@ def plot_ttt_ti64(context, input_path: str) -> None:
 
 @graph
 def ttt_ti64_graph():
+    """ The two ops above as a Graph """
+    # pylint: disable=no-value-for-parameter
     plot_ttt_ti64(run_ttt_ti64())
 
 
